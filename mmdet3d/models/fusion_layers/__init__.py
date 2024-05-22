ccf39c92245d4f6953e7fbb82638beb0f218775e
Tom Sanitz <tom.sanitz@googlemail.com>
2024-05-22 09:21:29 +0200

    add cross_attention_v2 that uses a skip connection around the cross attention

--- mmdet3d/models/fusion_layers/__init__.py
+++ mmdet3d/models/fusion_layers/__init__.py
@@ -5,9 +5,10 @@ from .point_fusion import PointFusion
 from .vote_fusion import VoteFusion
 from .concat_fusion import ConcatFusion
 from .cross_attention_fusion import MultiHeadCrossAttention
+from .cross_attention_fusion_v2 import MultiHeadCrossAttentionV2
 
 __all__ = [
     'PointFusion', 'VoteFusion', 'apply_3d_transformation',
     'bbox_2d_transform', 'coord_2d_transform',"ConcatFusion",
-    "MultiHeadCrossAttention"
+    "MultiHeadCrossAttention", "MultiHeadCrossAttentionV2"
 ]
