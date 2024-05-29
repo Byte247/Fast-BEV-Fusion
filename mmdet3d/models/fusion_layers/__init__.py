0febdd6ac782973d692eeaad2fdf96e644717a06
Tom Sanitz <tom.sanitz@googlemail.com>
2024-05-29 13:56:50 +0200

    create "add_fusion" baseline

--- mmdet3d/models/fusion_layers/__init__.py
+++ mmdet3d/models/fusion_layers/__init__.py
@@ -4,6 +4,7 @@ from .coord_transform import (apply_3d_transformation, bbox_2d_transform,
 from .point_fusion import PointFusion
 from .vote_fusion import VoteFusion
 from .concat_fusion import ConcatFusion
+from .add_fusion import AddFusion
 from .cross_attention_fusion import MultiHeadCrossAttention
 from .cross_attention_fusion_v2 import MultiHeadCrossAttentionV2
 from .cross_attention_fusion_more_voxels import MultiHeadCrossAttentionMoreCamVoxels
@@ -13,5 +14,6 @@ __all__ = [
     'PointFusion', 'VoteFusion', 'apply_3d_transformation',
     'bbox_2d_transform', 'coord_2d_transform',"ConcatFusion",
     "MultiHeadCrossAttention", "MultiHeadCrossAttentionV2",
-    "MultiHeadCrossAttentionMoreCamVoxels","MultiHeadCrossAttentionV2FlippedSkip"
+    "MultiHeadCrossAttentionMoreCamVoxels","MultiHeadCrossAttentionV2FlippedSkip",
+    "AddFusion"
 ]
