42a6c1f7ec8a5345cf7e6a0e4cb5264b949b86c9
Tom Sanitz <tom.sanitz@googlemail.com>
2024-06-14 23:01:05 +0200

    wip

--- configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model_augmentations.py
+++ configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model_augmentations.py
@@ -278,7 +278,7 @@ test_pipeline = [
 
 
 data = dict(
-    samples_per_gpu=1,
+    samples_per_gpu=2,
     workers_per_gpu=4,
     train=dict(
         type='CBGSDataset',
@@ -315,7 +315,8 @@ data = dict(
 optimizer = dict(type='AdamW', lr=1e-4,
                  weight_decay=0.01,
                  paramwise_cfg=dict(
-                 custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}))
+                 custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0),
+                              'neck_3d': dict(lr_mult=0.5, decay_mult=1.0)}))
 # max_norm=10 is better for SECOND
 optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
 
