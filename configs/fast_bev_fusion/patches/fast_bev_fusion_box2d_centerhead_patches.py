f7ab34db5e1739fb70b2f76a7c2ad5d92db96565
Tom Sanitz <tom.sanitz@googlemail.com>
2024-06-16 20:53:02 +0200

    wip

--- configs/fast_bev_fusion/patches/fast_bev_fusion_box2d_centerhead_patches.py
+++ configs/fast_bev_fusion/patches/fast_bev_fusion_box2d_centerhead_patches.py
@@ -280,7 +280,7 @@ optimizer = dict(type='AdamW', lr=1e-4,
                  paramwise_cfg=dict(
                  custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}))
 # max_norm=10 is better for SECOND
-optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
 # learning policy
 lr_config = dict(
