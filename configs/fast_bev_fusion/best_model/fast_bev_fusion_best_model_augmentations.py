9a23eefa04eb608b3dec9317b74ac0e06cdb2b71
Tom Sanitz <tom.sanitz@googlemail.com>
2024-06-18 09:50:42 +0200

    wip

--- configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model_augmentations.py
+++ configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model_augmentations.py
@@ -312,11 +312,11 @@ data = dict(
         test_mode=True,
         box_type_3d='LiDAR'))
 
-optimizer = dict(type='AdamW', lr=1e-4,
+optimizer = dict(type='AdamW', lr=1e-5,
                  weight_decay=0.01,
                  paramwise_cfg=dict(
                  custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0),
-                              'neck_3d': dict(lr_mult=0.05, decay_mult=1.0)})) #try to combat nan even more
+                              'neck_3d': dict(lr_mult=0.5, decay_mult=1.0)})) #try to combat nan even more
 # max_norm=10 is better for SECOND
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
