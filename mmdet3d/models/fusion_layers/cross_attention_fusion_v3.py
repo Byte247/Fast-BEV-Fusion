9f364476a278c1c0f53910697c303c95fda4804a
Tom Sanitz <tom.sanitz@googlemail.com>
2024-06-21 14:50:07 +0200

    wip

--- mmdet3d/models/fusion_layers/cross_attention_fusion_v3.py
+++ mmdet3d/models/fusion_layers/cross_attention_fusion_v3.py
@@ -226,5 +226,4 @@ class MultiHeadCrossAttentionV3(nn.Module):
 
         output = self.upsample_layer_act(self.upsample_layer_norm(self.upsample_layer(output)))
 
-        print(output.shape)
         return output
