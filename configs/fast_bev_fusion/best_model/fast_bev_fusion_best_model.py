c400f9fcd30a1f1df59b2692375054c935d4549b
Tom Sanitz <tom.sanitz@googlemail.com>
2024-06-24 16:54:20 +0200

    wip

--- configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model.py
+++ configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model.py
@@ -303,7 +303,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=20)
 #total_epochs = 20
 checkpoint_config = dict(interval=1)
 log_config = dict(
-    interval=500,
+    interval=100,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook'),
