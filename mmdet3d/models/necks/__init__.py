f615942fa87b85e0342c319c13aa015adcb7deb5
Tom Sanitz <tom.sanitz@googlemail.com>
2024-06-28 22:00:23 +0200

    add m2bev with leakRelu to combat nan

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -5,6 +5,7 @@ from .second_fpn import SECONDFPN
 from .m2bev_neck import *
 from .fpn_with_cp import *
 from .rpn import RPNV3, RPNV4
+from .m2bev_neck_v2 import M2BevNeckLeakyRelu
 
 
-__all__ = ['FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'FPNWithCP', 'RPNV3', 'RPNV4']
+__all__ = ['FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'FPNWithCP', 'RPNV3', 'RPNV4', "M2BevNeckLeakyRelu"]
