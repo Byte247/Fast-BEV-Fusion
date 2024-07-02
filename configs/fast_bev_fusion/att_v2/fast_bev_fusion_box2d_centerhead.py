4bc59b6705550b82151157eaec13444d5bac0722
Tom Sanitz <tom.sanitz@googlemail.com>
2024-07-02 15:30:06 +0200

    wip

--- configs/fast_bev_fusion/att_v2/fast_bev_fusion_box2d_centerhead.py
+++ configs/fast_bev_fusion/att_v2/fast_bev_fusion_box2d_centerhead.py
@@ -275,12 +275,12 @@ data = dict(
         box_type_3d='LiDAR'))
 
 optimizer = dict(type='AdamW', lr=1e-4,
-                 weight_decay=0.1,
+                 weight_decay=0.05,
                  paramwise_cfg=dict(
                  custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0),
                               'pos_embed_camera': dict(lr_mult= 1.0, decay_mult=0.),
                               'pos_embed_lidar': dict(lr_mult= 1.0, decay_mult=0.),
-                              'fusion_module': dict(lr_mult= 1.0, decay_mult=0.25)})) #try to combat nan even more
+                              'fusion_module': dict(lr_mult= 1.0, decay_mult=0.5)})) #try to combat nan even more
 # max_norm=10 is better for SECOND
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
