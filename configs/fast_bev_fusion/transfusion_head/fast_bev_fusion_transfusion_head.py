2e7b4df4df87cb6aba1a8426fe0d94800124a056
Tom Sanitz <tom.sanitz@googlemail.com>
2024-07-04 12:40:56 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_transfusion_head.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_transfusion_head.py
@@ -106,43 +106,9 @@ model = dict(
         loss_heatmap=dict(type='GaussianFocalLoss', reduction='mean', loss_weight=1.0),
     ),
     
-    bbox_head_2d=dict(
-        type='FCOSHead',
-        num_classes=10,
-        in_channels=64,
-        stacked_convs=2,
-        feat_channels=32,
-        strides=[4, 8, 16, 32],
-        regress_ranges=((-1, 64), (64, 128), (128, 256), (256, 1e8)),
-        loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0),
-        loss_bbox=dict(type='IoULoss', loss_weight=1.0),
-        loss_centerness=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
     
     camera_n_voxels=(256, 256, 6), 
     camera_voxel_size=[0.4, 0.4, 1],
-
-    # training and testing settings for 2d
-    train_cfg_2d=dict(
-        assigner=dict(
-            type='MaxIoUAssigner',
-            pos_iou_thr=0.5,
-            neg_iou_thr=0.4,
-            min_pos_iou=0,
-            ignore_iof_thr=-1),
-        allowed_border=-1,
-        pos_weight=-1,
-        debug=False),
-    test_cfg_2d=dict(
-        nms_pre=1000,
-        min_bbox_size=0,
-        score_thr=0.05,
-        nms=dict(type='nms', iou_threshold=0.5),
-        max_per_img=100),
 
     # model training and testing settings for the head
     train_cfg=dict(
