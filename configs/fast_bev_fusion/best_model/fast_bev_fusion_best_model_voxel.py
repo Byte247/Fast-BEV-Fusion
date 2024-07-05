5ec904dd3c3070767875f89f80f1a26f773dc3c0
Tom Sanitz <tom.sanitz@googlemail.com>
2024-07-05 13:54:14 +0200

    wip

--- configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model_voxel.py
+++ configs/fast_bev_fusion/best_model/fast_bev_fusion_best_model_voxel.py
@@ -279,11 +279,10 @@ data = dict(
         test_mode=True,
         box_type_3d='LiDAR'))
 
-optimizer = dict(type='AdamW', lr=1e-4,
+optimizer = dict(type='AdamW', lr=1e-5,
                  weight_decay=0.01,
                  paramwise_cfg=dict(
                  custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0),
-                              'neck_3d': dict(lr_mult=0.1, decay_mult=1.0),
                               'pos_embed_camera': dict(lr_mult= 1.0, decay_mult=0.),
                               'pos_embed_lidar': dict(lr_mult= 1.0, decay_mult=0.)})) #try to combat nan even more
 
