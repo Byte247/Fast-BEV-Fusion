98013d2de235b5068ae28042c2d8880cd6a6432c
Tom Sanitz <tom.sanitz@googlemail.com>
2024-07-21 19:03:48 +0200

    wip

--- configs/fast_bev_fusion/no_neck/fast_bev_fusion_no_neck_point_and_image_augmentations.py
+++ configs/fast_bev_fusion/no_neck/fast_bev_fusion_no_neck_point_and_image_augmentations.py
@@ -340,7 +340,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=20)
 #total_epochs = 20
 checkpoint_config = dict(interval=1)
 log_config = dict(
-    interval=100,
+    interval=200,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook'),
