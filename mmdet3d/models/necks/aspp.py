81b7e31cb7fc677f6e5fad6ec3f67ed4f9bd3a7f
Tom Sanitz <tom.sanitz@googlemail.com>
2024-07-30 15:55:13 +0200

    wip

--- mmdet3d/models/necks/aspp.py
+++ mmdet3d/models/necks/aspp.py
@@ -85,7 +85,7 @@ class ASPPNeck(BaseModule):
         self.conv1x1 = nn.Conv2d(
             in_channels, in_channels, kernel_size=1, stride=1, bias=False, padding=0)
         
-        self.post_conv = ConvBlock(in_channels * 6, in_channels, kernel_size=1, stride=1, norm_cfg=self.norm_cfg)
+        self.post_conv = ConvBNRelu(in_channels * 6, in_channels, kernel_size=1, stride=1, norm_cfg=self.norm_cfg)
 
         self.branch1 = ASPPConv(in_channels, in_channels, dilation=1, norm_cfg=self.norm_cfg)
         self.branch6 = ASPPConv(in_channels, in_channels, dilation=6, norm_cfg=self.norm_cfg)
