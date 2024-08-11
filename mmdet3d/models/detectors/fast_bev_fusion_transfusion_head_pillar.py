3bd2486ae58e650e861531df65c4405a480e1b49
Tom Sanitz <tom.sanitz@googlemail.com>
2024-08-11 14:52:20 +0200

    wip

--- mmdet3d/models/detectors/fast_bev_fusion_transfusion_head_pillar.py
+++ mmdet3d/models/detectors/fast_bev_fusion_transfusion_head_pillar.py
@@ -467,12 +467,11 @@ class FastBEVFusionTransfusionheadPillar(BaseDetector):
         lidar_features = self.extract_pts_feat(points)
 
         #fuse lidar BEV and camera BEV features
-        feature_bev = self.fusion_module(lidar_features[0], feature_bev[0])
-        feature_bev =[feature_bev]
+        feature_bev = self.fusion_module(lidar_features, feature_bev)
 
 
         if self.bbox_head is not None:
-            outs = self.bbox_head(feature_bev, features_2d, img_metas)
+            outs = self.bbox_head(feature_bev)
             bbox_list = self.bbox_head.get_bboxes(outs, img_metas, rescale=False)
                                     
             bbox_results = [bbox3d2result(bboxes, scores, labels)for bboxes, scores, labels in bbox_list]
