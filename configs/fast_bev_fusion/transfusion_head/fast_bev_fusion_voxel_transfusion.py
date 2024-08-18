cc7d523615da63e477f08900dff2faa51c359c7f
Tom Sanitz <tom.sanitz@googlemail.com>
2024-08-18 09:23:17 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
@@ -333,7 +333,7 @@ optimizer = dict(type='AdamW', lr=1e-5,
                   paramwise_cfg=dict(
                   custom_keys={'fusion_module': dict(lr_mult=10.0, decay_mult=1.0),
                                'pos_embed_camera': dict(lr_mult=1.0, decay_mult=.0),
-                               'pos_embed_lidar': dict(lr_mult=1.0, decay_mult=.0)}))
+                               'pos_embed_lidar': dict(lr_mult=10.0, decay_mult=.0)}))
 
 
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
