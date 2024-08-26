84bfd122ddf245638b34b8cb718ff5f0252fc9a1
Tom Sanitz <tom.sanitz@googlemail.com>
2024-08-26 12:16:24 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion.py
@@ -323,7 +323,7 @@ data = dict(
         test_mode=True,
         box_type_3d='LiDAR'))
 
-optimizer = dict(type='AdamW', lr=1e-4,
+optimizer = dict(type='AdamW', lr=1e-5,
                   weight_decay=0.01,
                   paramwise_cfg=dict(
                   custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0),
@@ -332,18 +332,18 @@ optimizer = dict(type='AdamW', lr=1e-4,
                                'pos_embed_lidar': dict(lr_mult=1.0, decay_mult=.0)}))
 
 
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2))
 lr_config = dict(
      policy='cyclic',
-     target_ratio=(10, 1e-4),
+     target_ratio=(10, 1e-5),
      cyclic_times=1,
-     step_ratio_up=0.3,
+     step_ratio_up=0.2,
  )
 momentum_config = dict(
      policy='cyclic',
      target_ratio=(0.85 / 0.95, 1),
      cyclic_times=1,
-     step_ratio_up=0.3,
+     step_ratio_up=0.2,
  )
 
 
