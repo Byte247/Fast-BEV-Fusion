5a91a26692aa30abd6402e56cca69efe28a9faf2
Tom Sanitz <tom.sanitz@googlemail.com>
2024-08-27 11:28:49 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
@@ -17,14 +17,15 @@ class_names = [
 model = dict(
     type='FastBEVFusionTransfusionheadVoxel',
     backbone=dict(
-        type='ResNet',
-        depth=50,
+        type='ResNeXt',
+        depth=101,
+        groups=64,
+        base_width=4,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         norm_eval=True,
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50'),
         style='pytorch',
         dcn=dict(type='DCN', deform_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)
@@ -33,9 +34,9 @@ model = dict(
         type='FPN',
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         in_channels=[256, 512, 1024, 2048],
-        out_channels=64,
+        out_channels=128,
         num_outs=4),
-    neck_fuse=dict(in_channels=256, out_channels=64),
+    neck_fuse=dict(in_channels=128*4, out_channels=64),
     neck_3d=dict(
         type='M2BevNeckTransOnly',
         is_transpose=False),
@@ -368,6 +369,6 @@ find_unused_parameters = True  # todo: fix number of FPN outputs
 log_level = 'INFO'
 #load_from = None
 load_additional_from = None
-load_from = 'https://download.openmmlab.com/mmdetection3d/v0.1.0_models/nuimages_semseg/cascade_mask_rcnn_r50_fpn_coco-20e_20e_nuim/cascade_mask_rcnn_r50_fpn_coco-20e_20e_nuim_20201009_124951-40963960.pth'
+load_from = 'https://download.openmmlab.com/mmdetection3d/v0.1.0_models/nuimages_semseg/htc_x101_64x4d_fpn_dconv_c3-c5_coco-20e_16x1_20e_nuim/htc_x101_64x4d_fpn_dconv_c3-c5_coco-20e_16x1_20e_nuim_20201008_211222-0b16ac4b.pth'
 resume_from = None
 workflow = [('train', 1)]
