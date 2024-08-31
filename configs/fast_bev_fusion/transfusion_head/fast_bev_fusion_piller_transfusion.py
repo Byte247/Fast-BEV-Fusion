4a0d8ffde0e84ce3f247486dd7d77f56e07f8ec8
Tom Sanitz <tom.sanitz@googlemail.com>
2024-08-31 11:13:46 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion.py
@@ -15,15 +15,14 @@ class_names = [
 model = dict(
     type='FastBEVFusionTransfusionheadPillar',
     backbone=dict(
-        type='ResNeXt',
-        depth=101,
-        groups=64,
-        base_width=4,
+        type='ResNet',
+        depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50'),
         style='pytorch',
         dcn=dict(type='DCN', deform_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)
