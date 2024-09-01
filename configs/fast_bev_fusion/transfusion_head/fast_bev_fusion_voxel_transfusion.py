e5c06027123177ecf2ac0fb433e70535a852ff2f
Tom Sanitz <tom.sanitz@googlemail.com>
2024-09-01 16:40:27 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
@@ -360,7 +360,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=20)
 #total_epochs = 20
 checkpoint_config = dict(interval=1)
 log_config = dict(
-    interval=50,
+    interval=250,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook'),
