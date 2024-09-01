26c5a1d44e0f0548b8e5a0516ab8d3cde7b31cf4
Tom Sanitz <tom.sanitz@googlemail.com>
2024-09-01 14:18:19 +0200

    increase batch size further

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_voxel_transfusion.py
@@ -296,7 +296,7 @@ test_pipeline = [
     dict(type='Collect3D', keys=['img','points'])]
 
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=8,
     workers_per_gpu=1,
     train=dict(
         type='CBGSDataset',
