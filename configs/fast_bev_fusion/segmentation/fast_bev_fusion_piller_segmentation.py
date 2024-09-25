8f754d932a502baf5bbe173a12d3dbaa3a0a3660
Tom Sanitz <tom.sanitz@googlemail.com>
2024-09-25 15:31:04 +0200

    wip

--- configs/fast_bev_fusion/segmentation/fast_bev_fusion_piller_segmentation.py
+++ configs/fast_bev_fusion/segmentation/fast_bev_fusion_piller_segmentation.py
@@ -276,8 +276,8 @@ data = dict(
 optimizer = dict(type='AdamW', lr=1e-4,
                   weight_decay=0.01,
                   paramwise_cfg=dict(
-                  custom_keys={'pos_embed_camera': dict(lr_mult=0.1, decay_mult=.0),
-                               'pos_embed_lidar': dict(lr_mult=0.1, decay_mult=.0)}))
+                  custom_keys={'pos_embed_camera': dict(lr_mult=1.0, decay_mult=.0),
+                               'pos_embed_lidar': dict(lr_mult=1.0, decay_mult=.0)}))
 
 
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
