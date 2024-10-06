1a1ff7d08a1b9093f4dd8f517c8dee0c32370873
Tom Sanitz <tom.sanitz@googlemail.com>
2024-10-06 08:58:41 +0200

    wip

--- mmdet3d/models/detectors/__init__.py
+++ mmdet3d/models/detectors/__init__.py
@@ -29,6 +29,7 @@ from .fast_bev_fusion_no_neck import FastBEVFusionNoNeck
 from .transfusion_head_pretrain import TransFusionHeadPretrain
 from .fast_bev_fusion_transfusion_head_dsvt import FastBEVFusionTransfusionheadDSVT
 from .transfusion_head_dsvt_pretrain import TransFusionHeadDSVT
+from .fast_bev_fusion_transfusion_head_pillar_robust import FastBEVFusionTransfusionheadPillarRobust
 
 
 __all__ = [
@@ -40,5 +41,5 @@ __all__ = [
     'FastBEVFusionCenterheadPretrained', 'FastBEVFusionCenterheadLarge',
     "FastBEVFusionCenterheadVoxel", "FastBEVFusionTransfusionheadPillar",
     "FastBEVFusionNoNeck", "FastBEVFusionTransfusionheadVoxel","TransFusionHeadPretrain",
-    "FastBEVFusionTransfusionheadDSVT", "TransFusionHeadDSVT"
+    "FastBEVFusionTransfusionheadDSVT", "TransFusionHeadDSVT", "FastBEVFusionTransfusionheadPillarRobust"
 ]
