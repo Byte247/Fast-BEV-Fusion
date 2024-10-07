b9ff2e37dbb7c6fd3da53b3ed692a1b68e110234
Tom Sanitz <tom.sanitz@googlemail.com>
2024-10-07 15:37:25 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_large.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_large.py
@@ -37,7 +37,7 @@ model = dict(
         in_channels=[256, 512, 1024, 2048],
         out_channels=64,
         num_outs=4),
-    neck_fuse=dict(in_channels=256, out_channels=128),
+    neck_fuse=dict(in_channels=256, out_channels=64),
     neck_3d=dict(
         type='M2BevNeckTransOnly',
         is_transpose=False),
