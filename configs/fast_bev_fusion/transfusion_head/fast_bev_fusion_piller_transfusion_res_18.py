7674cc598449a89634a6ab431cf2b93206592c4e
Tom Sanitz <tom.sanitz@googlemail.com>
2024-10-08 17:13:34 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_res_18.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_res_18.py
@@ -357,7 +357,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=20)
 #total_epochs = 20
 checkpoint_config = dict(interval=1)
 log_config = dict(
-    interval=1000,
+    interval=100,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook'),
--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_robust.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_robust.py
@@ -77,7 +77,7 @@ model = dict(
 
     bbox_head=dict(
         type='TransFusionHead',
-        num_proposals=200,
+        num_proposals=500,
         auxiliary=True,
         in_channels=384,
         hidden_channel=128,
@@ -179,9 +179,9 @@ model = dict(
             pc_range=point_cloud_range[:2],
             out_size_factor=out_size_factor,
             voxel_size=[0.2, 0.2],
-            nms_type=None,
-            pre_max_size=1000,
-            post_max_size=83,
+            nms_type='rotate',
+            pre_maxsize=1000,
+            post_maxsize=83,
             nms_thr=0.2)
             
 )
