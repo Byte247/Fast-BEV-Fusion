7b89393b9bc69a3aaaf3937584ea399110c1a3df
Tom Sanitz <tom.sanitz@googlemail.com>
2024-10-10 14:18:18 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_res_34.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_res_34.py
@@ -249,9 +249,7 @@ train_pipeline = [
         type='MultiViewPipeline',
         n_images=6,
         transforms=[
-            dict(type='LoadImageFromFile'),
-            dict(type='Resize', img_scale=(1600, 900), keep_ratio=True),
-            dict(type='Pad', size_divisor=32)]),
+            dict(type='LoadImageFromFile')]),
     dict(type='RandomAugImageMultiViewImage', data_config=data_config),
     dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
     dict(type='ObjectRangeFilter', point_cloud_range=point_cloud_range),
