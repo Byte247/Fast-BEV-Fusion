3390d0363e04f1eff914a1f7756b3994793335c8
Tom Sanitz <tom.sanitz@googlemail.com>
2024-10-20 14:47:21 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_res_18_less_embed.py
+++ configs/fast_bev_fusion/transfusion_head/fast_bev_fusion_piller_transfusion_res_18_less_embed.py
@@ -377,4 +377,4 @@ load_from = './work_dirs/backbones/cascade_mask_rcnn_r18.pth'
 workflow = [('train', 1)]
 
 # fp16 settings, the loss scale is specifically tuned to avoid Nan
-fp16 = dict(loss_scale='dynamic')
+#fp16 = dict(loss_scale='dynamic')
