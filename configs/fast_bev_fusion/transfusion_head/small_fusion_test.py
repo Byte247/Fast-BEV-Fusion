7a73e4351a75616887d7b6b54e04c6b338cd41c6
Tom Sanitz <tom.sanitz@googlemail.com>
2024-10-23 16:04:40 +0200

    wip

--- configs/fast_bev_fusion/transfusion_head/small_fusion_test.py
+++ configs/fast_bev_fusion/transfusion_head/small_fusion_test.py
@@ -304,8 +304,8 @@ test_pipeline = [
 
 
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=4,
     train=dict(
         type='CBGSDataset',
         dataset=dict(
